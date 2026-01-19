595c3b05c4f7a003a6e8545e5baacfd929b1b2cd
Airshock72 <gelagela2222@gmail.com>
2026-01-19 20:51:02 +0400

    simple examples of modules in node js

---
+++ 4-names.js
@@ -0,0 +1,6 @@
+const privateName = 'THIS IS PRIVATE NAME MUST BE HIDDEN'
+
+const john = 'john'
+const peter = 'peter'
+
+module.exports = { john, peter }
---
+++ 7-mind-grenade.js
@@ -0,0 +1,8 @@
+const num1 = 7
+const num2 = 3
+
+const sumNumbers = () => {
+    return num1 + num2
+}
+
+console.log("The sum is:", sumNumbers())
