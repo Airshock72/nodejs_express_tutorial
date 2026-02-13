5c2ed983a22553a8902839b5ba73b4af9ad01cc1
Airshock72 <gelagela2222@gmail.com>
2026-02-13 17:20:52 +0400

    added express app.js simple server

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end('<h1>Homepage</h1>')
+})
+
+server.listen(5000)
