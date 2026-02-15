9f2a1d9537150d3ac431bfa78877450d04295535
Airshock72 <gelagela2222@gmail.com>
2026-02-15 18:12:01 +0400

    Add routing with homepage HTML file and 404 handling

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,8 +1,21 @@
 const http = require('http')
+const { readFileSync } = require('fs')
+
+//get all Files
+const homepage = readFileSync('./index.html', 'utf8')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('<h1>Homepage</h1>')
+    const url = req.url
+    if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(homepage)
+    } else if (url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end('<h1>About Page</h1>')
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' })
+        res.end('<h1>404 Page Not Found</h1>')
+    }
 })
 
 server.listen(5000)
